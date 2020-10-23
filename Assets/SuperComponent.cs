738379277352aea6c9f0b2861c8d54390eecb54e
Slaerd <38158233+Slaerd@users.noreply.github.com>
2020-10-23 03:26:50 +0200

    Hide and Show

---
+++ Assets/SuperComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SuperComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent hideSubComponent;
+    public UnityEvent showSubComponent;
+    public MeshRenderer m_renderer;
+    public virtual void Start()
+    {
+        m_renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        if (m_renderer.isVisible)
+            hideSubComponent.Invoke();
+        else
+            showSubComponent.Invoke();
+    }
+}
---
+++ Assets/WinScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : SuperComponent
+{
+    // Start is called before the first frame update
+    
+    override public void Start()
+    {
+        Debug.Log("You're winner");
+    }
+
+    // Update is called once per frame
+    override public void Update()
+    {
+        
+    }
+}
